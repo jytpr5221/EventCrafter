ea332903629f38fc87f4a1c5a114b44ad49d9d09
Jyoti Prakash Panda <147559530+jytpr5221@users.noreply.github.com>
2024-12-05 02:43:25 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -10,12 +10,7 @@ const orgRouter = require('./src/organizers/routes/organizer.js');
 const eventRouter = require('./src/events/routes/events.js');
 const customerRouter = require('./src/customer/routes/customer.js');
 const { restrictToLoggedInUsers } = require('./src/middlewares/middleware.js');
-// console.log(databaseUrl)
-app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
 app.use(express.json());
 app.use(cors());
 app.use(express.urlencoded({ extended: false }));
